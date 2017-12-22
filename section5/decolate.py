2b548e9e57175f0cebc5a5a8cc443850d39d27ed
hyuraku <yu@hill-wood.com>
2017-12-22 19:16:13 +0900

    write new code
    to modify the code

--- section5/decolate.py
+++ section5/decolate.py
@@ -1,6 +1,22 @@
+def print_info(func):
+    def wrapper(*args,**kwargs):
+        print('start')
+        result=func(*args,**kwargs)
+        print('end')
+        return result
+    return wrapper
+
+@print_info
 def add_num(a,b):
     return a+b
 
+r=add_num(10,20)
+print(r)
+
+'''
 print('start')
-print(add_num(10,20))
+r=add_num(10,20)
 print('end')
+
+print(r)
+'''
