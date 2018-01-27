4b2b3f77cbbfb51312c11b178610d6a7a6c4a19f
hyuraku <yu@hill-wood.com>
2018-01-27 19:51:44 +0900

    study inheritance and property

--- section7/inheritance.py
+++ section7/inheritance.py
@@ -16,18 +16,30 @@ class Person(object):
 
 
 class Teacher(Person):
-    def __init__(self,name='Tim',subject='Japanese',age=30):
+    def __init__(self,name='Tim',subject='Japanese',age=30,place='A_City'):
         #self.name = name
         super().__init__(name)
         self.subject=subject
         # _ をつけることで外部からの変更を防ぐ
+        #ただ書き換え可能にする方法はある
         self._age = age
+        self.__place = place
 
     #上のageに対してのプロパティ
     @property
     def age(self):
         return self._age
 
+    # _ をつけた変数を書き換えにするコマンド
+    @age.setter
+    def age(self, how_years_old):
+        if how_years_old >= 30:
+            self._age = how_years_old
+        else:
+            raise ValueError
+
+    def where_live(self):
+        print('I live in {}'.format(self.__place))
     def teach(self):
         print('I teach {}'.format(self.subject))
     def say_age(self):
@@ -43,3 +55,5 @@ teacher.say_some()
 #下のコマンドはエラーになる。
 teacher.age =25
 teacher.say_age()
+
+teacher.where_live()
