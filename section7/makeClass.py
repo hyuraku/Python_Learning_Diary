4be48784817860a456ad603ba1b25de45dcbd71a
hyuraku <yu@hill-wood.com>
2018-01-31 23:53:00 +0900

    delete the method
    delete the method I wrote in 'inheritance.py'
    
     Please enter the commit message for your changes. Lines starting

--- section7/makeClass.py
+++ section7/makeClass.py
@@ -1,36 +1,30 @@
-#カッコ内にはpbjectを書いたほうがよい。
-#別に書かなくてもpython3では動く
+# カッコ内にはpbjectを書いたほうがよい。
+# 別に書かなくてもpython3では動く
+
+
 class Person(object):
-    #コンストラクタ:オブジェクト生成時に実行されるメソッド
+    # コンストラクタ:オブジェクト生成時に実行されるメソッド
     #__inin__
-    def __init__(self,name='Mike'):
-        #自身に値を保持している
-        self.name=name
+    def __init__(self, name='Mike'):
+        # 自身に値を保持している
+        self.name = name
 
     def say_some(self):
-        #selfをつけて、保持している値にアクセスしている。
+        # selfをつけて、保持している値にアクセスしている。
         print('I am {}'.format(self.name))
-        #class内のメソッドもselfでアクセスできる。
+        # class内のメソッドもselfでアクセスできる。
         self.run()
 
     def run(self):
         print('run')
 
-    #デストラクタ:オブジェクトが使われなくなったときに実行されるメソッド
+    # デストラクタ:オブジェクトが使われなくなったときに実行されるメソッド
     #__del__
     def __del__(self):
         print('good bye')
 
 
-
-class Teacher(Person):
-    def __init__(self,subject='Japanese'):
-        self.subject=subject
-
-    def teach(self):
-        print('I teach {}'.format(self.subject))
-
-#クラス生成
+# クラス生成
 person = Person()
 person.say_some()
 #>I am Mike
