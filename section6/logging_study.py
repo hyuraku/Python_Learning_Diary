c625cef11d1cc1f940a4bf559485467520eb24d8
hyuraku <yu@hill-wood.com>
2018-02-12 18:33:59 +0900

    fix the file
    learn new thing about logging and its format

--- section6/logging_study.py
+++ section6/logging_study.py
@@ -1,20 +1,26 @@
 import logging
 
-#このコマンドを実行しても有効にならなった。
-logging.basicConfig(filename='test.log',level=logging.INFO)
+#logのフォーマットを変えることができる
+formatter = '%(levelname)s:%(message)s'
+#下のコマンドはlogの日時を表示させる。
+formatter = '%(asctime)s:%(message)s'
+
+# デフォルトではwarning以上しか出力されない。
+#basicConfigのlevelの設定により、ファイル(test.log)にinfoまで出力された。
+#filenameを指定することで別ファイルに保存可能
+logging.basicConfig(filename='test.log',level=logging.INFO,format=formatter)
 
 logging.critical('critical')
 logging.error('error')
 logging.warning('warning')
-logging.info('info')
+#formatを使わなくても以下のように式展開が可能になる。
+logging.info('info　%s %s','test1','test2')
 logging.debug('debug')
 
-# デフォルトではwarning以上しか出力されない。
-#basicConfigのlevelの設定により、ファイル(test.log)にinfoまで出力された。
 """
 >
 CRITICAL:root:critical
 ERROR:root:error
 WARNING:root:warning
-INFO:root:info
+INFO:root:info　test1 test2
 """
