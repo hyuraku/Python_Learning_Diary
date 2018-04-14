c4a1be80c05b414711826dc266f20a444a99d80d
hyuraku <yu@hill-wood.com>
2018-04-14 15:55:48 +0900

    dbmのファイル修正
    コマンドの解説文を追加

--- section7/dbm_test.py
+++ section7/dbm_test.py
@@ -1,8 +1,10 @@
 import dbm
 
+#dbを書き込みできる状態で開く
 # with dbm.open('cache','c') as db:
 #     db['key1']='value1'
 #     db['key2']='value2'
 
+#dbを読み込みできる状態で開く
 with dbm.open('cache','r') as db:
     print(db.get('key1'))
