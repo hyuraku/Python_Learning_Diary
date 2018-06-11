19bc1d0b99d8490950d447673944f49c419208bd
hyuraku <yu@hill-wood.com>
2018-06-11 23:28:40 +0900

    unittestのセットアップについて学んだ

--- section9/unittest_lesson/unittest_exam.py
+++ section9/unittest_lesson/unittest_exam.py
@@ -3,14 +3,19 @@ import unittest
 import calculation
 
 class Caltest(unittest.TestCase):
+    def setUp(self):
+        print('setup')
+        self.cal = calculation.Cal()
+
     def test_add_num_and_double(self):
-        cal=calculation.Cal()
-        self.assertEqual(cal.add_num_and_double(1,1),4)
+        # cal=calculation.Cal()
+        self.assertEqual(self.cal.add_num_and_double(1,1),4)
 
     def test_add_num_and_double_raise(self):
-        cal=calculation.Cal()
+        # cal=calculation.Cal()
+        #例外処理にはwithステートメントを使う
         with self.assertRaises(ValueError):
-            cal.add_num_and_double('1','1'),
+            self.cal.add_num_and_double('1','1'),
 
 if __name__ == "__main__":
     unittest.main()
