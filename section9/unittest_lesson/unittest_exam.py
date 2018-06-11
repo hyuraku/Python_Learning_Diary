1177d08bb31c246c6a6df97dccffef6d772c82b7
hyuraku <yu@hill-wood.com>
2018-06-11 23:27:10 +0900

    例外処理に関するunittestを学んだ

--- section9/unittest_lesson/unittest_exam.py
+++ section9/unittest_lesson/unittest_exam.py
@@ -3,9 +3,14 @@ import unittest
 import calculation
 
 class Caltest(unittest.TestCase):
-    def test_add_num_and_doubl(self):
+    def test_add_num_and_double(self):
         cal=calculation.Cal()
         self.assertEqual(cal.add_num_and_double(1,1),4)
+
+    def test_add_num_and_double_raise(self):
+        cal=calculation.Cal()
+        with self.assertRaises(ValueError):
+            cal.add_num_and_double('1','1'),
 
 if __name__ == "__main__":
     unittest.main()
