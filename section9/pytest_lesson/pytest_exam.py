18bb3cb28f7f18f01410259b7970c309aed4eea5
hyuraku <yu@hill-wood.com>
2018-06-16 17:49:49 +0900

    pytestのsetup,teardownについての学習

--- section9/pytest_lesson/pytest_exam.py
+++ section9/pytest_lesson/pytest_exam.py
@@ -3,14 +3,20 @@ import pytest
 import calculation
 
 class TestCal(object):
+    def setup_method(self,method):
+        print('method = {}'.format(method.__name__))
+        self.cal = calculation.Cal()
+
+    def teardown_method(self,method):
+        print('method = {}'.format(method.__name__))
+        del self.cal
+
     def test_add_num_and_double(self):
-        cal = calculation.Cal()
-        assert cal.add_num_and_double(1,1) == 4
+        assert self.cal.add_num_and_double(1,1) == 4
 
     def test_add_num_and_double_raise(self):
         with pytest.raises(ValueError):
-            cal = calculation.Cal()
-            cal.add_num_and_double('1','1')
+            self.cal.add_num_and_double('1','1')
 
 # 実行時のコマンドは下記の通り
 # python -m pytest [ファイル名]
