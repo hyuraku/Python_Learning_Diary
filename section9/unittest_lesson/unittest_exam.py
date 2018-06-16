2d8379d80df3cb19a1e616211c3fc66ef2b5e88d
hyuraku <yu@hill-wood.com>
2018-06-16 17:33:58 +0900

    unittestのスキップについての学習

--- section9/unittest_lesson/unittest_exam.py
+++ section9/unittest_lesson/unittest_exam.py
@@ -2,6 +2,8 @@ import unittest
 
 import calculation
 
+release_name = 'lesson'
+
 class Caltest(unittest.TestCase):
     def setUp(self):
         print('setup')
@@ -11,6 +13,8 @@ class Caltest(unittest.TestCase):
         print('clean up')
         del self.cal
 
+    # @unittest.skip('skip!')
+    @unittest.skipIf(release_name == 'lesson','skip!!')
     def test_add_num_and_double(self):
         # cal=calculation.Cal()
         self.assertEqual(self.cal.add_num_and_double(1,1),4)
@@ -19,7 +23,7 @@ class Caltest(unittest.TestCase):
         # cal=calculation.Cal()
         #例外処理にはwithステートメントを使う
         with self.assertRaises(ValueError):
-            self.cal.add_num_and_double('1','1'),
+            self.cal.add_num_and_double('1','1')
 
 if __name__ == "__main__":
     unittest.main()
