1b820cd5d6cb3f735333541a0d091c6e3a43851a
hyuraku <yu@hill-wood.com>
2018-06-16 17:46:22 +0900

    pytestの例外処理についての学習

--- section9/pytest_lesson/pytest_exam.py
+++ section9/pytest_lesson/pytest_exam.py
@@ -1,3 +1,5 @@
+import pytest
+
 import calculation
 
 class TestCal(object):
@@ -5,6 +7,10 @@ class TestCal(object):
         cal = calculation.Cal()
         assert cal.add_num_and_double(1,1) == 4
 
+    def test_add_num_and_double_raise(self):
+        with pytest.raises(ValueError):
+            cal = calculation.Cal()
+            cal.add_num_and_double('1','1')
 
 # 実行時のコマンドは下記の通り
 # python -m pytest [ファイル名]
