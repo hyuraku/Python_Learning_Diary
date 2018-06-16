45ee50a6c1c0d218df983857e9eafd72dc3cbb08
hyuraku <yu@hill-wood.com>
2018-06-16 17:58:48 +0900

    テスト開始時にsetupするよう設定

--- section9/pytest_lesson/pytest_exam.py
+++ section9/pytest_lesson/pytest_exam.py
@@ -3,13 +3,24 @@ import pytest
 import calculation
 
 class TestCal(object):
+
+    @classmethod
+    def setup_class(cls):
+        print('start')
+        cls.cal = calculation.Cal()
+
+    @classmethod
+    def teardown_class(cls):
+        print('end')
+        del cls.cal
+
     def setup_method(self,method):
         print('method = {}'.format(method.__name__))
-        self.cal = calculation.Cal()
+        # self.cal = calculation.Cal()
 
     def teardown_method(self,method):
         print('method = {}'.format(method.__name__))
-        del self.cal
+        # del self.cal
 
     def test_add_num_and_double(self):
         assert self.cal.add_num_and_double(1,1) == 4
