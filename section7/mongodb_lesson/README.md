54daf25dc0fb52024cfb31ceaefa70b23f2bed7d
hyuraku <yu@hill-wood.com>
2018-07-04 23:49:35 +0900

    MongoDBの学習

---
+++ section7/mongodb_lesson/README.md
@@ -0,0 +1,4 @@
+立ち上げるときは以下のコマンドを入力する
+```
+mongod --dbpath ./mongodb_lesson/db
+```
---
+++ section7/mongodb_lesson/mongo_test.py
@@ -0,0 +1,34 @@
+import datetime
+
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['test_database']
+
+stack1 = {
+    'name':'customer1',
+    'pip':['python','java','go'],
+    'info':{'os':'mac'},
+    'date': datetime.datetime.utcnow()
+}
+
+stack2 = {
+    'name':'customer2',
+    'pip':['python','ruby','go'],
+    'info':{'os':'linux'},
+    'date': datetime.datetime.utcnow()
+}
+
+db_stacks = db.stacks
+stack_id = db_stacks.insert_one(stack1).inserted_id
+print(stack_id,type(stack_id))
+from bson.objectid import ObjectId
+str_stack_id = '5b3cdc0cbbca0ab9451fa1fa'
+print("###########")
+print(db_stacks.find_one({'_id':stack_id}))
+print(db_stacks.find_one({'_id':ObjectId(str_stack_id)}))
+# >>5b3cdc0cbbca0ab9451fa1fa <class 'bson.objectid.ObjectId'>
+# ###########
+# {'_id': ObjectId('5b3cdc0cbbca0ab9451fa1fa'), 'name': 'customer1', 'pip': ['python', 'java', 'go'], 'info': {'os': 'mac'}, 'date': datetime.datetime(2018, 7, 4, 14, 39, 8, 881000)}
+# {'_id': ObjectId('5b3cdc0cbbca0ab9451fa1fa'), 'name': 'customer1', 'pip': ['python', 'java', 'go'], 'info': {'os': 'mac'}, 'date': datetime.datetime(2018, 7, 4, 14, 39, 8, 881000)}
