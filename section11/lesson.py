51ba5117ce1db22a5ed50a8782e10670af72be58
hyuraku <yu@hill-wood.com>
2018-12-06 00:26:45 +0900

    encrypt one file

--- section11/lesson.py
+++ section11/lesson.py
@@ -22,4 +22,18 @@ print(cipher_text)
 
 cipher2 = AES.new(key, AES.MODE_CBC,iv)
 decprypt_text = cipher2.decrypt(cipher_text)
-print(decprypt_text[:decprypt_text[-1]])
+print(decprypt_text[:-decprypt_text[-1]])
+
+with open('plaintext','r') as f, open('enc.dat','wb') as e:
+    plain_file = f.read()
+    cipher = AES.new(key, AES.MODE_CBC,iv)
+    padding_length = AES.block_size - len(plain_file) % AES.block_size
+    plain_file += chr(padding_length) * padding_length
+
+    cipher_text = cipher.encrypt(plain_file)
+    e.write(cipher_text)
+
+with open('enc.dat','rb') as f:
+    cipher2 = AES.new(key, AES.MODE_CBC,iv)
+    decprypt_text = cipher2.decrypt(f.read())
+    print(decprypt_text[:-decprypt_text[-1]].decode('utf-8'))
