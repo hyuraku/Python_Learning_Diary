e2507415d7d7193edfe74da1e50ffca3b8243fcf
hyuraku <yu@hill-wood.com>
2019-01-15 23:46:29 +0900

    use hashlib.pbkdf2_hmac

--- section11/hashlib_lesson.py
+++ section11/hashlib_lesson.py
@@ -2,20 +2,26 @@ import hashlib
 import base64
 import os
 
-print(hashlib.sha256(b'test').hexdigest())
-
 user_name = 'user1'
 user_pass = 'password'
 db = {}
 
-def get_digest(password):
-    password = bytes(password, 'utf-8')
-    digest = hashlib.sha256(password).hexdigest()
-    return digest
+salt = base64.b64encode(os.urandom(32))
 
-db[user_name] = get_digest(user_pass)
+# def get_digest(password):
+#     password = bytes(password, 'utf-8')
+#     digest = hashlib.sha256(salt + password).hexdigest()
+#     for _ in range(10000):
+#         digest = hashlib.sha256(bytes(digest, 'utf-8')).hexdigest()
+#     return digest
+
+# db[user_name] = get_digest(user_pass)
+
+digest = hashlib.pbkdf2_hmac('sha256', bytes(user_pass, 'utf-8'),salt,10000)
+db[user_name] = digest
 
 def is_login(user_name, password):
-    return get_digest(password) == db[user_name]
+    digest = hashlib.pbkdf2_hmac('sha256', bytes(user_pass, 'utf-8'),salt,10000)
+    return digest == db[user_name]
 
 print(is_login(user_name, user_pass))
